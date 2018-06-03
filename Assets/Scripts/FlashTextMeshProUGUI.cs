8823a93dbee8489e9aa07c776f133105d9d75900
SUGAMA Tatsuo <gama@sstinc.co.jp>
2018-06-03 21:13:12 +0900

    initial commit

---
+++ Assets/Scripts/FlashTextMeshProUGUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlashTextMeshProUGUI : MonoBehaviour {
+
+    public float interval = 0.5f;
+    float nextTime;
+    TextMeshProUGUI textmeshPro;
+    float alpha = 0;
+    // Use this for initialization
+    void Start () {
+        if (interval <= 0f) {
+            interval = 0.5f;
+            nextTime = interval;
+        }
+        textmeshPro = GetComponent<TextMeshProUGUI>();
+        alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var current = Time.time;
+        if (current > nextTime) {
+            nextTime = current + interval;
+
+            var color = textmeshPro.faceColor;
+
+            byte a;
+            if (color.a < 128) {
+                a = 255;
+            } else {
+                a = 0;
+            }
+            textmeshPro.faceColor = new Color32(
+                color.r,
+                color.g,
+                color.b,
+                a);
+        }
+	}
+}
