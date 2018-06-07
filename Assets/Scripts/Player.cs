135a6aec082d8c37894f3b31a18fde3717220429
SUGAMA Tatsuo <gama@sstinc.co.jp>
2018-06-08 07:06:22 +0900

    プレイヤーが移動できて弾をうつところまで

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    AxisChange fireAxis;
+
+    static string horizontalAxis = "Horizontal";
+    static string verticalAxis = "Vertical";
+
+    public float Speed = 1f;
+
+    public GameObject shotPrefab;
+
+    void Start () {
+        fireAxis = new AxisChange("Fire1");
+    }
+
+    void Update () {
+        // プレイヤーの移動
+        float delta = Time.deltaTime;
+        var h = Input.GetAxis(horizontalAxis);
+        var v = Input.GetAxis(verticalAxis);
+
+        var old = transform.localPosition;
+        transform.localPosition = new Vector3(
+            old.x + h * delta * Speed,
+            old.y + v * delta * Speed,
+            old.z);
+
+        // shot入力チェック
+        fireAxis.Update();
+        if (fireAxis.ChangePositive) {
+            
+            var shotPosition = transform.localPosition;
+            shotPosition.x += 0.8f;
+            shotPosition.y += 0.05f;
+            // プレハブからインスタンスを生成
+            Instantiate (shotPrefab, shotPosition, Quaternion.identity);
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+	public Vector3 deltaVector = new Vector3(10, 0, 0); 
+
+	private Renderer renderer;
+	void Start () {
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update () {
+		var delta = Time.deltaTime;
+		transform.localPosition += deltaVector * delta;
+
+		if (!renderer.isVisible) {
+    		Destroy(this.gameObject);
+  		}
+	}
+}
